a274a4c626525fead089e8aeb2a00f7657614fdb
mThanuj <thanujmullaguri1825@gmail.com>
2024-05-12 05:49:54 +0530

    mThanuj

---
+++ Stack/Monotonic Stack/next_smaller_element.py
@@ -0,0 +1,25 @@
+from Stack import stack
+
+
+def solve(arr: list[int]) -> list[int]:
+    stk = stack.Stack()
+    res = [-1] * len(arr)
+
+    for i in range(len(arr) - 1, -1, -1):
+        while not stk.is_empty() and stk.peek() >= arr[i]:
+            stk.pop()
+
+        if stk.is_empty():
+            res[i] = -1
+        else:
+            res[i] = stk.peek()
+
+        stk.push(arr[i])
+
+    return res
+
+
+if __name__ == '__main__':
+    arr = [13, 7, 6, 12]
+
+    print(solve(arr.copy()))
---
+++ Stack/stack.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self, data: int, bottom=None):
+        self.data = data
+        self.bottom = bottom
+
+
+class Stack:
+    def __init__(self):
+        self.top = None
+        self.cur_size = 0
+
+    def push(self, data: int) -> None:
+        new_node = Node(data, self.top)
+        self.top = new_node
+        self.cur_size += 1
+
+    def pop(self) -> int | float:
+        if not self.is_empty():
+            data = self.top.data
+            self.top = self.top.bottom
+            self.cur_size -= 1
+            return data
+
+        return float('inf')
+
+    def peek(self) -> int | float:
+        if not self.is_empty():
+            return self.top.data
+
+        return float('inf')
+
+    def size(self) -> int:
+        return self.cur_size
+
+    def is_empty(self) -> bool:
+        return self.top is None
+
+    def print(self) -> None:
+        temp = self.top
+        while temp:
+            print(temp.data)
+            temp = temp.bottom
+
+        print("None")
