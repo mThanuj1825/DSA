3ad9fe20ab9eadc8bafbddeb0a0c51a665314516
mThanuj <thanujmullaguri1825@gmail.com>
2024-05-12 08:03:09 +0530

    mThanuj

--- Stack/Implementations_of_Stack/min_stack.py
+++ Stack/Implementations_of_Stack/min_stack.py
@@ -1,16 +1,17 @@
 class Node:
-    def __init__(self, data: int, bottom=None):
+    def __init__(self, data: int, cur_min: int | float, nxt=None):
         self.data = data
-        self.bottom = bottom
+        self.cur_min = min(data, cur_min)
+        self.next = nxt
 
 
-class Stack:
+class MinStack:
     def __init__(self):
         self.top = None
         self.cur_size = 0
 
     def push(self, data: int) -> None:
-        new_node = Node(data, self.top)
+        new_node = Node(data, self.top.cur_min if self.top else float('inf'), self.top)
         self.top = new_node
         self.cur_size += 1
 
@@ -21,13 +22,19 @@ class Stack:
             self.cur_size -= 1
             return data
 
-        return float('inf')
+        return float('-inf')
 
     def peek(self) -> int | float:
         if not self.is_empty():
             return self.top.data
 
-        return float('inf')
+        return float('-inf')
+
+    def get_min(self) -> int | float:
+        if not self.is_empty():
+            return self.top.cur_min
+
+        return float('-inf')
 
     def size(self) -> int:
         return self.cur_size
